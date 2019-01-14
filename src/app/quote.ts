9fc3acd29563aaec07931956101e74c9999a85fd
papi <ryanmuuo91@gmail.com>
2019-01-14 22:34:16 +0300

    add changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,13 @@
 export class Quote {
+  public showDescription:boolean
   constructor(
     public id:number,
     public name:string,
     public description:string,
-    public personSubmitting:string){
-      
+    public personSubmitting:string,
+    public newDate: Date){
+
+this.showDescription=false
     }
+
 }
